ae0be5ea99d271bd1b52877e7492603ca733b039
TUM-08 <arthur.nsp.tum@gmail.com>
2025-10-06 09:33:08 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -39,8 +39,11 @@ function App() {
           </header>
           <div className="busca-box">
             <div className="busca-container">
-              <input type="text" />
-            <button></button>
+              <input
+               type="text"
+               placeholder="Digite o nome da cidade.."
+               />
+            <button>Buscar</button>
             </div>
 
           </div>
